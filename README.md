ee6dd9a19c9d97a034cbad0e3f0050e7c7b42ba8
topherdan1 <chris@Chriss-MBP.fios-router.home>
2015-08-13 21:34:23 -0400

    First prototype with an alert reminder and lotus icon

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Focus Extension
 
 This is a simple extension that helps you focus. When you open a potentially distracting website, it asks you to pause for a few seconds and focus on what you are doing. Then you can go to the website. The momentary pause is meant to prevent compulsively switching tabs and checking distracting websites.
+
+# Credit
+
+Special thanks to Callum Taylor for the Lotus icon. 
--- focus.js
+++ focus.js
@@ -1,19 +1,3 @@
-// When the extension is installed or upgraded ...
-chrome.runtime.onInstalled.addListener(function() {
-  // Replace all rules ...
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    // With a new rule ...
-    chrome.declarativeContent.onPageChanged.addRules([
-      {
-        // That fires when a page's URL contains gmail
-        conditions: [
-          new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: { urlContains: 'mail.google.com' },
-          })
-        ],
-        // And shows the extension's page action.
-        actions: [ new chrome.declarativeContent.ShowPageAction() ]
-      }
-    ]);
-  });
-});
+// Update the styles for pages that need focus:
+
+alert('Inhale............Exhale...............');
